337baca54957aea714b37038d6d32f5467dff3f3
Anthony Udezi <tonywaka@uniben.edu>
2018-06-06 22:06:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {prop: 1}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  recipes.key = value
+  return recipes
+}
+updateObjectWithKeyAndValue(obj, 'prop2', 2)
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  recipes
+}
+destructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', 2)
+
+function deleteFromObjectByKey(object, key) {
+  recipes
+}
+deleteFromObjectByKey(obj, 'prop')
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  recipes
+}
+destructivelyDeleteFromObjectByKey(obj, 'prop')
